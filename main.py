8a1eb636fbbb750fa422c7da7dd7f4362b76c1b0
nuo18 <119784955+nuo18@users.noreply.github.com>
2023-04-02 15:59:01 +0700

    First Commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import tkinter
+# For GUI
+from tkinter import *
+
+# Colours
+Black = "#253649"
+Teal = "57aaa0"
+
+root = Tk()
+root.title("Chatbot")
+root.config(background=Black, width=500, height=600)
+
+root.mainloop()
