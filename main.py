9e1484fb74cc582153119f124af529fd7c1ab6ce
nuo18 <119784955+nuo18@users.noreply.github.com>
2023-04-04 19:19:02 +0700

    Second Commit

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 import tkinter
-import customtkinter  # <- import the CustomTkinter module
-
-root_tk = tkinter.Tk()  # create the Tk window like you normally do
-root_tk.geometry("400x240")
-root_tk.title("CustomTkinter Test")
-
-def button_function():
-    print("button pressed")
-
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=root_tk, corner_radius=10, command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+import customtkinter as cs  # <- import the CustomTkinter module
+import server, client
 
 # Colours
-Black = "#253649"
+Black = "#282424"
 Teal = "57aaa0"
 
-root_tk.config(background=Black, width=500, height=600)
+root = tkinter.Tk()  # create the Tk window like you normally do
+root.geometry("500x600")
+root.title("Simple Chat")
+root.config(background=Black)
 
-root_tk.mainloop()
+root.mainloop()
